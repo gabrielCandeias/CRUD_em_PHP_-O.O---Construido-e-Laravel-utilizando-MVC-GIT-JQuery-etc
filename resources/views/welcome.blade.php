02690819fe13b8e5fa95c4d1cb81077e68ac7811
Gabriel Candeias Barbosa <gabrielcandeias41@gmail.com>
2022-03-21 09:16:50 -0300

    Instalação inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Index')
+
+@section('content')
+
+@extends('layouts.nav')
+
+<h1>olá</h1>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\SystemController;
+
+Route::get('/', [SystemController::class, 'index']);
+
+Route::get('/system/create', [SystemController::class, 'create']);
